45f72eb39a772e1246cd57784b9c5f439d42273d
alfarouq18 <alfarouqm18@gmail.com>
2026-06-13 23:05:46 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var map = new Dictionary<string, List<string>>();
+        foreach(string s in strs){
+            int[] arr = new int[26];
+            for(int i = 0; i < s.Length; i++){
+                arr[s[i] - 'a']++;
+            }
+            string key = string.Join("," , arr);
+            if(!map.ContainsKey(key)){
+                map[key] = new List<string>();
+            }
+            map[key].Add(s);
+        }
+        return map.Values.ToList<List<string>>();
+    }
+}
