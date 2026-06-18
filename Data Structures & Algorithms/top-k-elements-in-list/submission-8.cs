4d56aec96b3aa43b12ce204171011ee71b085cb2
alfarouq18 <alfarouqm18@gmail.com>
2026-06-18 01:37:44 -0400

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        var dict = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++){
+            dict[nums[i]] = dict.GetValueOrDefault(nums[i], 0) + 1;
+        }
+
+        List<int>[] arr = new List<int>[nums.Length + 1];
+        for(int i = 0; i < arr.Length; i++){
+            arr[i] = new List<int>();
+        }
+
+        foreach(var num in dict){
+            arr[num.Value].Add(num.Key);
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+
+        for(int i = arr.Length -1; i > 0; i--){
+            if(index >= k){
+                break;
+            }
+            if(arr[i].Count == 0){
+                continue;
+            }
+            foreach(int l in arr[i]){
+                res[index] = l;
+                index++;
+            }
+        }
+        return res;
+    }
+}
