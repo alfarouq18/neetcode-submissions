a50dd0bb25056e861a7ad7be23bd9d16bdc04383
alfarouq18 <alfarouqm18@gmail.com>
2026-07-14 22:30:38 -0400

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()){
+            return false;
+        }
+        int[] s1CharArr = new int[26];
+        int[] s2CharArr = new int[26];
+
+        for(int i = 0; i < s1.length(); i++){
+            s1CharArr[s1.charAt(i) - 'a']++;
+            s2CharArr[s2.charAt(i) - 'a']++;
+        }
+        if(Arrays.equals(s1CharArr, s2CharArr)){
+            return true;
+        }
+        int l = 0;
+        for(int r = s1.length(); r < s2.length(); r++){
+            s2CharArr[s2.charAt(l) - 'a']--;
+            s2CharArr[s2.charAt(r) - 'a']++;
+            if(Arrays.equals(s1CharArr, s2CharArr)){
+                return true;
+            }
+            l++;
+        }
+        return false;
+    }
+}
