3527dc252e29217d022c416fdda6aab71388ea44
alfarouq18 <alfarouqm18@gmail.com>
2026-07-25 16:52:30 -0400

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while(l <= r){
+            int middle = (r - l) / 2 + l;
+
+            if(nums[middle] > target){
+                r = middle - 1;
+            }else if(nums[middle] < target){
+                l = middle + 1;
+            }else{
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
